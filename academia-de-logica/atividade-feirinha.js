b3e23e5c8b30c8039112d52b1fde993f7f57c303
Gabriel Ferrari Ribeiro <gabrielferrari601@gmail.com>
2021-11-19 14:45:54 -0300

    Cria a atividade da academia de lógica do dia 19-11-21

---
+++ academia-de-logica/atividade-feirinha.js
@@ -0,0 +1,33 @@
+// ENTRADA
+// 1) guardar as frutas
+// 2) guardar o preço com e sem promoção
+// 3) guardar a fruta que está em promoção
+// 4) guardar o preço individual de cada fruta
+// 5) guardar 2 caixas contendo frutas sortidas
+// PROCESSAMENTO
+// 6) Confirmar que as maçãs estão em promoção
+// 7) Passar automaticamente fruta por fruta
+// SAÍDA
+// :usando_óculos_escuros: A cada passada pela fruta mostrar ela na tela.
+// -----------------------------------------------
+// Abaixo o gabarito da 1 e da 5 para ajudar com as outras:
+// 1) guardar as frutas
+// -> Criar dado <fruta1> guardar um conteudo em formato de texto <maça>
+// 5) guardar 2 caixas contendo frutas sortidas
+// -> Guardar chamado de <caixa1> guardando multiplos dados <fruta1> <fruta2> <fruta3> <fruta4> <fruta5>
+// Responder…
+var fruta1 = "maca"
+var fruta2 = "pera"
+var fruta3 = "banana"
+var fruta4 = "manga"
+var fruta5 = "laranja"
+var preço1 = 2
+var preço2 = 2 - (2*0.1)
+var frutas = [{nome:fruta1, valor: preco2}, {nome:fruta2, valor:preço1},{nome:fruta3, valor:preco1},{ nome:fruta4, valor:preco1},{nome:fruta5, valor:preco1}]
+var caixa1 = Math
+
+
+
+
+
+
