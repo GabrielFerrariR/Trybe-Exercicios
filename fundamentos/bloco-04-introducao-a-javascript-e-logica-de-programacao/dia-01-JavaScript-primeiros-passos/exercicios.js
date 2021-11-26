e6431569d6db064e97ad3f768c1b51c6e8b44fac
Gabriel Ferrari Ribeiro <gabrielferrari601@gmail.com>
2021-11-26 18:28:53 -0300

    Resoluçao dos dois primeiros requisitos

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-JavaScript-primeiros-passos/exercicios.js
@@ -0,0 +1,15 @@
+const a = 5
+const b = 10
+let soma = a + b
+let subt = a - b
+let mult = a * b
+let div = a / b
+let modl = a % b
+
+if ( a > b){
+  console.log ('a é maior que b');
+} else if ( a < b){
+  console.log('a é menor que b');
+} else {
+  console.log('os valores sao iguais')
+}
