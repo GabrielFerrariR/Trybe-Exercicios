5f2bbc7b7cf356aac04b843bd124851343f942e0
Gabriel Ferrari Ribeiro <gabrielferrari601@gmail.com>
2021-11-26 18:39:20 -0300

    Adiciona as questoes tres e quatro

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-JavaScript-primeiros-passos/exercicios.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-JavaScript-primeiros-passos/exercicios.js
@@ -1,15 +1,34 @@
 const a = 5
 const b = 10
-let soma = a + b
-let subt = a - b
-let mult = a * b
-let div = a / b
-let modl = a % b
+// let soma = a + b
+// let subt = a - b
+// let mult = a * b
+// let div = a / b
+// let modl = a % b
 
-if ( a > b){
-  console.log ('a é maior que b');
-} else if ( a < b){
-  console.log('a é menor que b');
-} else {
-  console.log('os valores sao iguais')
+// if ( a > b){
+//   console.log ('a é maior que b');
+// } else if ( a < b){
+//   console.log('a é menor que b');
+// } else {
+//   console.log('os valores sao iguais')
+// }
+//Exercicio 3
+const valor1 = -48
+const valor2 = 95
+const valor3 = 15
+if (valor1 > valor2 && valor1 > valor3) {
+  console.log(`${valor1} é o maior valor`)
+} else if(valor2 > valor3 && valor2 > valor1){
+  console.log(`${valor2} é o maior valor`)
+}else{
+  console.log(`${valor3} é o maior valor`)
+}
+//Exercicio 4
+if(valor1 > 0){
+  console.log(`${valor1} é um valor positivo`)
+} else if(valor1<0){
+  console.log(`${valor1} é um valor negativo`)
+}else {
+  console.log(`${valor1} é igual a 0`)
 }
