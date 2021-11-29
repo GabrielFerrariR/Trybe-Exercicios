32d23010897259f41f9911c1aff0ef2c02db33d9
Gabriel Ferrari Ribeiro <gabrielferrari601@gmail.com>
2021-11-29 15:56:03 -0300

    Adiciona os exercicios praticos do conteudo teorico

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-JavaScript-Array-e-loop-for/array.js
@@ -0,0 +1,12 @@
+// Exercicio1
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
+//Exercicio2
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
+//Exercicio3
+menu.push('Contato')
+console.log(menu)
