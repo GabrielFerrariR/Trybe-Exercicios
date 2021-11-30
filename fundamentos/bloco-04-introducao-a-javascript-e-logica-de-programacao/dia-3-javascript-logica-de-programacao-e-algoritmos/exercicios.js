3d671b11f6ca9063c496563f3dfd34d967cc5adf
Gabriel Ferrari Ribeiro <gabrielferrari601@gmail.com>
2021-11-30 20:00:51 -0300

    Adiciona os exercicios do dia

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios.js
@@ -0,0 +1,42 @@
+// 1- Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n .
+let n = 4
+let nString = ''
+for (let i = 0; i < n; i += 1) {
+  nString += '*'
+}
+for (let i = 0; i < n; i += 1) {
+  if (n > 1) {
+    console.log(nString)
+  } else {
+    console.log('Insira um valor de n maior que 1')
+  }
+}
+// 2 -Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base.
+console.log('------------------')
+
+let nString2 = ''
+if (n > 1) {
+  for (let i = 0; i < n; i += 1) {
+    nString2 += '*'
+    console.log(nString2)
+  }
+} else {
+  console.log('Insira um valor de n maior que 1')
+}
+
+//3- Agora inverta o lado do triângulo.
+console.log('------------------')
+
+let nString3 = ''
+let position = n
+for (let index = 0; index < n; index += 1) {
+  for (let i = 0; i < n; i += 1) {
+    nString3 += ' '
+    if (i == position) {
+      nString3 += '*'.repeat(index)
+      console.log(nString3)
+    }
+  }
+  nString3 = ''
+  position -= 1
+}
