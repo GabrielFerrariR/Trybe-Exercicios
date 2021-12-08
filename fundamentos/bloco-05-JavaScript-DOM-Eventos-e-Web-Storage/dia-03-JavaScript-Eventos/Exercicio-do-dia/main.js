c94ac56cc131519cbded8d350e556a85878985db
Gabriel Ferrari Ribeiro <gabrielferrari601@gmail.com>
2021-12-08 13:24:59 -0300

    Adiciona o exercicio

--- fundamentos/bloco-05-JavaScript-DOM-Eventos-e-Web-Storage/dia-03-JavaScript-Eventos/Exercicio-do-dia/main.js
+++ fundamentos/bloco-05-JavaScript-DOM-Eventos-e-Web-Storage/dia-03-JavaScript-Eventos/Exercicio-do-dia/main.js
@@ -1,3 +1,17 @@
+function createDaysOfTheWeek() {
+  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+  const weekDaysList = document.querySelector('.week-days');
+  for (let index = 0; index < weekDays.length; index += 1) {
+    const days = weekDays[index];
+    const dayListItem = document.createElement('li');
+    dayListItem.innerHTML = days;
+
+    weekDaysList.appendChild(dayListItem);
+  };
+};
+
+createDaysOfTheWeek();
+
 const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 const daysUl = document.getElementById('days');
 
@@ -26,36 +40,36 @@ function createBtn(string) {
 }
 createBtn('Feriados')
 const button = document.getElementById('btn-holiday')
-button.addEventListener('click', colorChange)
-
+button.addEventListener('click', colorChange);
 function colorChange(){
   const holiday = document.getElementsByClassName('holiday')
   for ( let i = 0; i < holiday.length; i += 1) {
-    holiday[i].style.backgroundColor = 'green'
-  }
-  button.addEventListener('click', colorChangeBack)
-}
-function colorChangeBack() {
-  const holiday = document.getElementsByClassName('holiday')
-  for ( let i = 0; i < holiday.length; i += 1) {
+    if (holiday[i].style.backgroundColor === 'green' ){
     holiday[i].style.backgroundColor = 'rgb(238,238,238)'
+    } else {
+    holiday[i].style.backgroundColor = 'green'
+    }
   }
-  button.addEventListener('click', colorChange)
 }
+function createFridayBtn(string){
+  const divButton = document.querySelector('.buttons-container');
+  const button = document.createElement('button');
+  button.id = "btn-friday"
+  button.innerText = string
+  divButton.appendChild(button)
+}
+createFridayBtn('Sexta-feira') 
+const FridayBtn = document.getElementById('btn-friday');
+FridayBtn.addEventListener('click', fridayTextChange)
 
-function createDaysOfTheWeek() {
-  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
-  const weekDaysList = document.querySelector('.week-days');
-
-  for (let index = 0; index < weekDays.length; index += 1) {
-    const days = weekDays[index];
-    const dayListItem = document.createElement('li');
-    dayListItem.innerHTML = days;
-
-    weekDaysList.appendChild(dayListItem);
-  };
-};
-
-createDaysOfTheWeek();
-
-// Escreva seu código abaixo.
+function fridayTextChange(){
+  const fridays = document.getElementsByClassName('friday')
+  const fridayDigits = [4, 11, 18, 25]
+  for ( let i = 0; i < fridays.length; i += 1) {
+    if (fridays[i].innerText === "Mufasa!") {
+      fridays[i].innerText = fridayDigits[i]
+    }
+    else {
+      fridays[i].innerText = "Mufasa!"}
+  }
+}
