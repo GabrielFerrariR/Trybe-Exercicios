64c7f46ec6ce2116783797d879be8542b40ec861
Gabriel Ferrari Ribeiro <gabrielferrari601@gmail.com>
2022-01-31 19:30:10 -0300

    Adiciona praticas e exercicios do dia

---
+++ fundamentos/bloco-9-javascript-e-testes-assincronos/dia-2-fetch-API-e-async-await/exercicios/criptomoedas/apiScript.js
@@ -0,0 +1,8 @@
+const API_URL = `https://api.coincap.io/v2/assets`
+
+const fetchCoins = () => {
+
+  fetch(API_URL, myObject)
+  .then((response) => console.log(response))
+}
+window.onload = () => fetchCoins
