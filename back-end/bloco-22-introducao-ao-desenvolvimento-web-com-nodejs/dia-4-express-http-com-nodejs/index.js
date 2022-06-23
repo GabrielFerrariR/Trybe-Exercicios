527444f22fb8212f1225d242b6f87f1f79fab657
Gabriel <gabrielferrari601@gmail.com>
2022-06-23 15:23:02 -0300

    exercicios do dia

---
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+
+const app = express();
+
+app.use(bodyParser.json());
+
+// app.get('/ping', (req, res) =>{
+//   res.status(200).json({ message: 'pong' });
+// })
+
+app.post('/hello', (req, res) => {
+  const { name } = req.body;
+  return res.status(200).json({ message: `Hello, ${name}!` });
+});
+
+app.post('/greetings', (req, res) => {
+  const { name, age } = req.body;
+  if (age > 17 ) return res.status(200).json({ message : `Hello, ${ name }`});
+  return res.status(401).json({message: "Unauthorized"})
+})
+
+app.listen(3001, () => {
+  console.log('Aplicação ouvindo na porta 3001');
+})
