615d5206ef016d884000005b6587fbe6b685db6f
Gabriel <gabrielferrari601@gmail.com>
2022-06-28 14:31:59 -0300

    Refactor: rest

---
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-3-arquitetura-web-rest-e-restful/hello-http-methods/exercicio/controllers/productController.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const ProductModel = require('../models/productModel');
+
+const app = express();
+app.use(express.json());
+
+const getAll =  async (req, res) => {
+  const products = await ProductModel.getAll();
+
+  res.status(200).json(products);
+};
+
+const getById = async (req, res) => {
+  const product = await ProductModel.getById(req.params.id);
+
+  return product;
+};
+
+const addProduct = async (req, res) => {
+  const { name, brand } = req.body;
+
+  const newProduct = await ProductModel.add(name, brand);
+
+  return newProduct;
+};
+
+const deleteProduct =  async (req, res) => {
+  const products = await ProductModel.exclude(req.params.id);
+
+  return products;
+};
+
+const updateProduct = async (req, res) => {
+  const { name, brand } = req.body;
+
+  const products = await ProductModel.update(req.params.id, name, brand);
+
+  return products;
+};
+
+module.exports = {
+  getAll,
+  getById,
+  addProduct,
+  deleteProduct,
+  updateProduct,
+};
